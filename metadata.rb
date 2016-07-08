5731a87c16c0db6cd002f36a9e3aa6bc48781c95
Blake Irvin <blake.irvin@gmail.com>
2016-07-08 20:29:22 +0200

    add github reference links for supermarket

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'blake.irvin@gmail.com'
 license 'MIT'
 description 'Installs/Configures habitat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
+source_url 'https://github.com/bixu/habitat-cookbook'
+issues_url 'https://github.com/bixu/habitat-cookbook/issues'
 
 depends 'tar'
