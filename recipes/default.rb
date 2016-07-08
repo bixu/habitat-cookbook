d93b6aa215f44bfc9c2b2540f1bae3e429b5421c
Blake Irvin <blake.irvin@gmail.com>
2016-07-08 19:42:17 +0200

    add logic to install the hab binary

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,16 @@
-#
-# Cookbook Name:: habitat-cookbook
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+version      = "#{node['habitat']['version']}"
+release      = "#{node['habitat']['release']}"
+architecture = "#{node['habitat']['architecture']}"
+kernel       = "#{node['habitat']['kernel']}"
+platform     = "#{architecture}-#{kernel}"
+
+include_recipe 'tar'
+
+tar_extract "https://bintray.com/habitat/stable/download_file?file_path=#{kernel}%2F#{architecture}%2Fhab-#{version}-#{release}-#{platform}.tar.gz" do
+  target_dir '/usr/local/bin'
+  creates '/usr/local/bin/hab'
+  tar_flags [ '--strip-components 1' ]
+end
+
+user 'hab' do
+end
