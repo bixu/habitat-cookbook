fd2a977491c0bdb1491e708f3ffef83656955b08
Blake Irvin <blake.irvin@gmail.com>
2016-07-08 19:40:27 +0200

    default attributes

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['habitat']['architecture'] = node['kernel']['processor']
+default['habitat']['kernel']       = 'linux'
+default['habitat']['release']      = '20160614230104'
+default['habitat']['version']      = '0.7.0'
