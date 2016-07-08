1517198c65fbb95261133a619e1ba76f7132a489
Blake Irvin <blake.irvin@gmail.com>
2016-07-08 20:18:50 +0200

    version bump for supermarket

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'blake.irvin@gmail.com'
 license 'MIT'
 description 'Installs/Configures habitat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 depends 'tar'
